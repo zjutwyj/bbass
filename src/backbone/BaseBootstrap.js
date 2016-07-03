154e2a1f373a5ce093b2da22ba5360c67be4f340
wyj <wyj@abbcc.net>
2016-07-03 09:02:16 +0800

    2016.07.03

--- src/backbone/BaseBootstrap.js
+++
@@ -1,8 +0,0 @@
-window.BaseModel = BaseModel;
-window.BaseCollection = BaseCollection;
-window.BaseItem = BaseItem;
-window.BaseDetail = BaseDetail;
-window.BaseList = BaseList;
-window.BaseService = BaseService;
-window.BaseView = BaseView;
-window.BaseUtils = BaseUtils;
--- src/backbone/BasePre.js
+++
@@ -1,7 +0,0 @@
-/**
- * @description 模块功能说明
- * @class ModuleName
- * @author yongjin<zjut_wyj@163.com> 2016/2/6
- */
-define('Bbass', [], function(require, exports, module){
-
